7842efcf508e2499460b309d70c715f20c2996fc
Isaiasfma <desenvolvimento1@novalar.com.br>
2023-10-27 14:15:57 -0400

    subindo

--- lib/view/aux_cadastrarlista.dart
+++ lib/view/aux_cadastrarlista.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:listadecomprass/custom/customText.dart';
 import 'package:listadecomprass/utils/colors.dart';
 
@@ -13,6 +15,8 @@ class _Aux_CadastrarListaState extends State<Aux_CadastrarLista> {
   final _formKey = GlobalKey<FormState>();
   bool _formValid = false;
   TextEditingController _nomeLista = TextEditingController();
+  TextEditingController _nomeItem = TextEditingController();
+  TextEditingController _quantidadeItem = TextEditingController();
 
   void _validateForm() {
     _formKey.currentState?.validate();
@@ -30,29 +34,64 @@ class _Aux_CadastrarListaState extends State<Aux_CadastrarLista> {
   void initState() {
     super.initState();
     _nomeLista.addListener(_validateForm);
+    _nomeItem.addListener(_validateForm);
+    _quantidadeItem.addListener(_validateForm);
   }
 
   @override
   void dispose() {
     super.dispose();
     _nomeLista.removeListener(_validateForm);
+    _nomeItem.removeListener(_validateForm);
+    _quantidadeItem.removeListener(_validateForm);
   }
 
   void _showAlertDialog() {
+    TextEditingController _nomeItemController = TextEditingController();
+    TextEditingController _quantidadeItemController = TextEditingController();
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Título do Alert'),
-          content: Text('Conteúdo do Alert'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Fechar'),
-            ),
-          ],
+          title: CustomText(title: 'Cadastrar Item',color: ColorsCoworking.textpage,),
+          content: Wrap(
+            children: [
+              Column(
+                children: [
+                  TextFormField(
+                    controller: _nomeItemController,
+                    decoration: InputDecoration(
+                      labelText: 'Nome do Item',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _quantidadeItemController,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    decoration: InputDecoration(
+                      labelText: 'Quantidade do Item',
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      String nomeItem = _nomeItemController.text;
+                      String quantidadeItem = _quantidadeItemController.text;
+                      Get.back();
+                    },
+                    child: Text('Cadastrar itens'),
+                  )
+                ],
+              ),
+            ],
+          ),
         );
       },
     );
@@ -80,7 +119,7 @@ class _Aux_CadastrarListaState extends State<Aux_CadastrarLista> {
                   controller: _nomeLista,
                   decoration: InputDecoration(
                     errorStyle:
-                    TextStyle(color: _formValid ? Colors.blue : Colors.red),
+                        TextStyle(color: _formValid ? Colors.blue : Colors.red),
                     errorBorder: OutlineInputBorder(
                       borderSide: BorderSide(
                         color: _formValid ? Colors.blue : Colors.red,
